d6a2d2e626061df2d62ef5cf65ecb13a14f54ac5
Jacob Holmes <jakeh1003@yahoo.com>
2021-10-09 20:52:50 -0400

    Update RubyController.cs

--- RubysAdventureProject/Assets/Scripts/RubyController.cs
+++ RubysAdventureProject/Assets/Scripts/RubyController.cs
@@ -7,8 +7,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+        
     }
 
     // Update is called once per frame
