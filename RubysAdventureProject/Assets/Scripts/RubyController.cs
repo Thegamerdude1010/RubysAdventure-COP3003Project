6d46ba551450427b8626394c163e5b870bdb93c4
Jacob Holmes <jakeh1003@yahoo.com>
2021-10-20 20:44:16 -0400

    Completed Section
    Completed the "World Interactions - Damage Zones and Enemies" section in the tutorial

--- RubysAdventureProject/Assets/Scripts/RubyController.cs
+++ RubysAdventureProject/Assets/Scripts/RubyController.cs
@@ -8,9 +8,13 @@ public class RubyController : MonoBehaviour  //this is a class that stores data 
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
 
     int currentHealth;
     public int health { get { return currentHealth; } } //this allows us to get Ruby's current health, it uses C# properties
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -29,6 +33,15 @@ public class RubyController : MonoBehaviour  //this is a class that stores data 
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -41,6 +54,15 @@ public class RubyController : MonoBehaviour  //this is a class that stores data 
     }
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
